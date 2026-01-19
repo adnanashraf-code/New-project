4b0c4ebd7f83a798e823c5c3d77260df5787698a
Adnan Ashraf <adnanashraf7205@gmail.com>
2026-01-20 02:19:12 +0530

    Paino

--- Js project 1.1/script.js
+++ Js project 1.1/script.js
@@ -1,7 +1,8 @@
 let main = document.querySelector("#main");
 let cursor = document.querySelector("#me");
+
 document.body.addEventListener("mousemove", function (dets) {
   cursor.style.left = dets.x + "px";
   cursor.style.top = dets.y + "px";
-  cursor.style.transform = "translate(-50%, -50%)";
+  
 });
